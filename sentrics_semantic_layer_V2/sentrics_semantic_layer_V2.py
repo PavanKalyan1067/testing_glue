be21c61dad71f7e7d86f870fbc149164a6fe703f
Pavan Kalyan Yellaboina <100332055+PavanKalyan1067@users.noreply.github.com>
2023-08-25 16:58:52 +0530

    Synchronizing job sentrics_semantic_layer_V2 from AWS Glue at 2023/08/25 11:28:52

---
+++ sentrics_semantic_layer_V2/sentrics_semantic_layer_V2.py
@@ -0,0 +1,46 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+# Script generated for node S3 bucket
+S3bucket_node1 = glueContext.create_dynamic_frame.from_options(
+    format_options={},
+    connection_type="s3",
+    format="parquet",
+    connection_options={
+        "paths": ["s3://test.processing.for.glue.prod/alarm/"],
+        "recurse": True,
+    },
+    transformation_ctx="S3bucket_node1",
+)
+
+alarm_df = S3bucket_node1.toDF()
+events_df = spark.read.parquet("s3://test.processing.for.glue.prod/events/")
+final_df1 = alarm_df.join(events_df,alarm_df.alarmid ==  events_df.alarmid,"left")
+final_df1.show(1)
+
+final_dynamic_frame = DynamicFrame.fromDF(final_df1, glueContext, "final_dynamic_frame")
+
+# Script generated for node S3 bucket
+S3bucket_node3 = glueContext.write_dynamic_frame.from_options(
+    frame=S3bucket_node1,
+    connection_type="s3",
+    format="glueparquet",
+    connection_options={
+        "path": "s3://test.processing.for.glue.prod.output/",
+    },
+    format_options={"compression": "uncompressed"},
+    transformation_ctx="S3bucket_node3",
+)
+
+job.commit()
